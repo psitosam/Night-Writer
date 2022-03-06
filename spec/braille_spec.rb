295412439a2ea742ffa1208f139bc680f2993638
Alex Psitos <apsitos08@gmail.com>
2022-03-05 19:36:43 -0500

    Test: Braille class first test

---
+++ spec/braille_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'simplecov'
+SimpleCov.start
+
+RSpec.describe Braille do
+  describe "#translate" do
+    it "turns a string of characters into an array of braille characters" do
+      incoming_text = "aa"
+      outgoing_text = subject.translate(incoming_text)
+      outgoing_array = [
+        [
+          ["0", "."],
+          [".", "."],
+          [".", "."],
+        ],
+        [
+          ["0", "."],
+          [".", "."],
+          [".", "."],
+        ]
+      ]
+      expect(outgoing_text).to eq(outgoing_array)
+    end
+  end 
