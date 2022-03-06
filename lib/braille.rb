bb1c95f3c62058030913b2f49f6d68aee4fa606f
Alex Psitos <apsitos08@gmail.com>
2022-03-05 19:52:57 -0500

    Trying to fix branch issue

--- lib/braille.rb
+++ lib/braille.rb
@@ -6,4 +6,14 @@ class Braille
       BrailleHash::DICTIONARY[char]
     end
   end
+
+  def render(input)
+    array = ["", "", ""]
+    input.each do |cell|
+      array[0].concat(cell[0].join)
+      array[1].concat(cell[1].join)
+      array[2].concat(cell[2].join)
+    end
+    array
+  end
 end
